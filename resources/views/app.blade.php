5b8282aab4e4c69eaeb6610f8a16d24e76ab7446
Shiham Samsudeen <shihams247@gmail.com>
2026-07-18 23:49:15 +0530

    fix: update page title to 'Zahira Booking Portal'

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title inertia>{{ config('app.name', 'Zahira Ground Booking') }}</title>
+    <title inertia>{{ config('app.name', 'Zahira Booking Portal') }}</title>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
